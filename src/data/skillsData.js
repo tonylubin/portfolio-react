3c503702101c141195f571d96689cd038f9eb2b7
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-03-31 12:53:36 +0100

    info for project & skills section

---
+++ src/data/skillsData.js
@@ -0,0 +1,70 @@
+const techSkills = [
+  {
+    title: "Html5",
+    size: "3x",
+    icon: "fa-brands fa-html5 fa-inverse",
+    iconColor: "html",
+  },
+  {
+    title: "Css3",
+    size: "3x",
+    icon: "fa-brands fa-css3-alt",
+    iconColor: "css3",
+  },
+  {
+    title: "Sass",
+    size: "3x",
+    icon: "fa-brands fa-sass",
+    iconColor: "sass",
+  },
+  {
+    title: "Javascript",
+    size: "3x",
+    icon: "fa-brands fa-js",
+    iconColor: "js",
+  },
+  {
+    title: "React",
+    size: "3x",
+    icon: "fa-brands fa-react",
+    iconColor: "react",
+  },
+  {
+    title: "Git",
+    size: "3x",
+    icon: "fa-brands fa-git-alt",
+    iconColor: "git",
+  },
+  {
+    title: "GitHub",
+    size: "3x",
+    icon: "fa-brands fa-github",
+    iconColor: "github",
+  },
+  {
+    title: "Npm",
+    size: "3x",
+    icon: "fa-brands fa-npm",
+    iconColor: "npm",
+  },
+  {
+    title: "Bootstrap",
+    size: "3x",
+    icon: "fa-brands fa-bootstrap",
+    iconColor: "bootstrap",
+  },
+  {
+    title: "NodeJs",
+    size: "3x",
+    icon: "fa-brands fa-node-js",
+    iconColor: "nodejs",
+  },
+  {
+    title: "Firebase",
+    size: "3x",
+    icon: "fa-solid fa-database",
+    iconColor: "firebase",
+  },
+];
+
+export default techSkills;
