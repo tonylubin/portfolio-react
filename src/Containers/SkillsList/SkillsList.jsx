1f8c2c73d153f9f07fdc37c343de8f9ce4fff216
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-04-02 21:48:15 +0100

    moved skillslist to container

---
+++ src/Containers/SkillsList/SkillsList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import SectionHeader from "../../Components/SectionHeader/SectionHeader";
+import SkillsCard from "../../Components/SkillsCard/SkillsCard";
+import styles from "./SkillsList.module.scss";
+import skillsCardData from "../../data/skillsData";
+
+const SkillsList = () => {
+
+  const getSkillsCards = skillsCardData.map((skillcard, index) => (
+
+    <SkillsCard
+      key={index}
+      icon={skillcard.icon}
+      iconColor={skillcard.iconColor}
+      title={skillcard.title}
+      size={skillcard.size}
+    />
+
+  ));
+
+  return (
+    <section className={styles.skillsList} id="skills">
+      <SectionHeader heading="Skills" />
+      <div className={styles.skillsList__container}>
+        {getSkillsCards}
+      </div>
+    </section>
+  );
+};
+
+export default SkillsList;
