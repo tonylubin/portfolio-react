24b7a2175aef4030c660820d986f43fe0d8cb4e0
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-04-02 21:47:27 +0100

    moved project showcase to container

---
+++ src/Containers/ProjectsShowcase/ProjectsShowcase.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import SectionHeader from "../../Components/SectionHeader/SectionHeader";
+import ProjectCard from "../../Components/ProjectCard/ProjectCard";
+import styles from "./ProjectsShowcase.module.scss";
+import cardData from "../../data/projectData.js";
+
+const ProjectsShowcase = () => {
+
+  const getProjects = cardData.map((card, index) => (
+
+    <ProjectCard
+      key={index}
+      title={card.title}
+      info={card.info}
+      imageUrl={card.imageUrl}
+      imageAlt={card.imageAlt}
+      isReversed={card.isReversed}
+      projectViewUrl={card.view}
+      projectCodeUrl={card.code}
+      techstackIcons={card.techstackIcons}
+    />   
+
+  ));
+
+  return (
+    <section className={styles.projectsDisplay} id="projects">
+      <SectionHeader heading="Projects" />
+      {getProjects}
+    </section>
+  );
+};
+
+export default ProjectsShowcase;
