4d3f9b5f821c87c9185c6de4d5b6fb22d239506c
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-04-04 14:33:24 +0100

    text update and margin

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,8 +9,13 @@ const About = () => {
       <div className={styles.aboutSection__textHolder}>
         <p>
           Hi there<span className={styles.wave}>&#128075;&#127996;</span>,let me introduce myself briefly, my name is Anthony Lubin and I am currently on a software development bootcamp course with Nology.
-          After my university studies, in London and Paris, I went on
-          to establish my own business in the automotive sector. Before embarking on my new journey of training to be a web developer I took the step of doing online coding courses to decide whether it would be the right direction to take. With a passion about languages and technology, I felt that the skills and mindset, garnered during my studies and work, would be compatable within the world of coding.      
+        </p>
+        <p>
+          After my university studies, in London and Paris, I went on to establish my own business in the automotive sector. Working in an ever-increasing technology driven environment it became apparent that one had to think along the lines of "why/how the software program is designed/functions?" in order to be effective and efficient (adapt/evolve or fade away).
+        </p>
+        <p>
+          This led me to explore the world of coding and, having a passion for tech and languages, with the aspect of building solutions and/or breaking down complex problems down into smaller or simplier processes in that "logic mindest" and with my guiding adoption of the "KISS(keep it simple stupid)" principle, it felt like a great blend of the two in my next career journey.<br />
+          Especially, as I believe in the motto of " you learn something new each day ", which the world of tech/code never fails to deliver!
         </p>
       </div>
     </section>
