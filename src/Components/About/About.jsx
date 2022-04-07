ec8188bd3a1bfe6b22feb9b726543bedd8945a07
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-04-07 11:18:34 +0100

    text

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           After my university studies, in London and Paris, I went on to establish my own business in the automotive sector. Working in an ever-increasing technology driven environment it became apparent that one had to think along the lines of "why/how the software program is designed/functions?" in order to be effective and efficient (adapt/evolve or fade away).
         </p>
         <p>
-          This led me to explore the world of coding and, having a passion for tech and languages, with the aspect of building solutions and/or breaking down complex problems down into smaller or simplier processes in that "logic mindest" and with my guiding adoption of the "KISS(keep it simple stupid)" principle, it felt like a great blend of the two in my next career journey.<br />
+          This led me to explore the world of coding and, having a passion for tech and languages, with the aspect of building solutions and/or breaking down complex problems down into smaller or simplier processes in that "logic mindest" felt like a great blend of the two in my next career journey.<br />
           Especially, as I believe in the motto of " you learn something new each day ", which the world of tech/code never fails to deliver!
         </p>
       </div>
