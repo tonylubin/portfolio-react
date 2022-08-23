3141f0ab7c547b501bc63a7e9a8878d4a349e837
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-08-23 12:35:26 +0100

    changed typing speed

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
       <div className={styles.landingPage__greeting}>
         <TypewriterComponent
           onInit={(typewriter) => {
-            typewriter.pauseFor(75)
+            typewriter.pauseFor(50).changeDelay(85)
             .typeString("<span style='font-weight: bold; font-size: clamp(1.9rem, 5vw, 2.5rem); color: #c3dcaf;'>Hello</span>, (tech world),</br>")
             .typeString("<span style='font-weight: bold'; color: #c3dcaf;'>I'm Anthony Lubin, </span>a junior web developer based in London. </br>")
             .typeString("</br><span style='font-weight: bold; color: #c3dcaf;'>Please </span>scroll down to</br>")
