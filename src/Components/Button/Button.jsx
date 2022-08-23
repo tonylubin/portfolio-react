4430abf5bb9856b79959734dd36dd8734f419024
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-08-23 12:41:16 +0100

    replace button image with skillIcon & align items

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,8 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import SkillIcon from "../SkillIcon/SkillIcon";
 import styles from "./Button.module.scss";
 
-const Button = ({ text, icon }) => {
+const Button = ({ text, width, height, viewBox, svgInfo, icon }) => {
 
   const destinationUrl = () => {
     console.log("you clicked me")
@@ -10,7 +10,7 @@ const Button = ({ text, icon }) => {
 
   return (
     <button className={styles.baseBtn} onClick={destinationUrl}>
-      <FontAwesomeIcon icon={icon} className={styles.iconBtn} />
+      <SkillIcon width={width} height={height} viewBox={viewBox} svgInfo={svgInfo} />
       {text}
     </button>
   )
