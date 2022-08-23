3c1692dd05bfaa927b01bdc0bdf0f6453bb9d2fa
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-08-23 12:42:22 +0100

    added new project

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,8 +4,44 @@ import translatorImg from "../assets/images/morsecode-laptopscreen.png";
 import battleshipsImg from "../assets/images/alienbattleships-laptopscreen.png";
 import myTodosImg from "../assets/images/mytodos-laptopscreen.png";
 import beerImg from "../assets/images/punkbeers-laptopscreen.png";
+import moneySaverImg from "../assets/images/money-saver-app.png";
+import { techSkills, expressSvg } from "./skillsData";
+
+
+// find index of icon in skills data
+const index = (skill) => {
+  let foundObj = techSkills.findIndex(skillObj => skillObj.title === skill);
+  return techSkills[foundObj];
+};
 
 const projectInfo = [
+  {
+    code: "https://github.com/tonylubin/webscraper---node-js.git",
+    view: "https://github.com/tonylubin/webscraper---node-js.git",
+    title: "Money Saver App",
+    info: "An web scraping app, using Puppeteer, that allows a user to set up a price alert for a product from the retailer Boots. Written entirely for the back-end in Node js with the server built on Express. Styled with Bootstrap and html pages server-side rendered with EJS. Alerts are stored & modelled with Mongoose & MongoDb. App is tasked scheduled (cron job) to run and sends a user an email when the product is cheaper.",
+    imageUrl: moneySaverImg,
+    imageAlt: "Screenshot of a webscraping app called Money saver app",
+    isReversed: false,
+    techstackIcons: [
+      {
+        viewBox: index("NodeJs").viewBox,
+        svgInfo: index("NodeJs").svgInfo
+      },
+      {
+        viewBox: index("MongoDb").viewBox,
+        svgInfo: index("MongoDb").svgInfo
+      },
+      {
+        viewBox: index("Bootstrap").viewBox,
+        svgInfo: index("Bootstrap").svgInfo
+      },
+      {
+        viewBox: expressSvg.viewBox,
+        svgInfo: expressSvg.svgInfo
+      }
+    ]
+  },
   {
     code: "https://github.com/tonylubin/Beer-Finder---Punk-Api---React",
     view: "https://tonylubin.github.io/Beer-Finder---Punk-Api---React",
@@ -13,22 +49,19 @@ const projectInfo = [
     info: 'A project built in React that allows the user to search for Punk beers, by letter/name or filter functionality, via the external Punk beer Api. The results are shown to the user as a flipable "beer card" with extra info about the beer on the back of the card. Project also incorporates, where suitable, unit testing using jest/enzyme.',
     imageUrl: beerImg,
     imageAlt: "Screenshot of a beer finder app using BrewDogs punk beers Api",
-    isReversed: false,
+    isReversed: true,
     techstackIcons: [
       {
-        size: "2xl",
-        icon: "fa-brands fa-react",
-        iconColor: "react",
+        viewBox: index("React").viewBox,
+        svgInfo: index("React").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-js",
-        iconColor: "js",
+        viewBox: index("Javascript").viewBox,
+        svgInfo: index("Javascript").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-sass",
-        iconColor: "sass",
+        viewBox: index("Sass").viewBox,
+        svgInfo: index("Sass").svgInfo
       },
     ],
   },
@@ -39,27 +72,23 @@ const projectInfo = [
     info: "An app for a user to keep track of tasks. Built with a connection to Firebase as a BaaS to allow CRUD operations, a user can write, update and delete a todo, as well as being able to show a todo as completed. Furthermore, authentication was added, thus to utilise the app a user must be registered.",
     imageUrl: myTodosImg,
     imageAlt: "Screenshot of a app that deals with todos",
-    isReversed: true,
+    isReversed: false,
     techstackIcons: [
       {
-        size: "2xl",
-        icon: "fa-brands fa-react",
-        iconColor: "react",
+        viewBox: index("React").viewBox,
+        svgInfo: index("React").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-js",
-        iconColor: "js",
+        viewBox: index("Javascript").viewBox,
+        svgInfo: index("Javascript").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-sass",
-        iconColor: "sass",
+        viewBox: index("Sass").viewBox,
+        svgInfo: index("Sass").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-solid fa-database",
-        iconColor: "firebase",
+        viewBox: index("Firebase").viewBox,
+        svgInfo: index("Firebase").svgInfo
       },
     ],
   },
@@ -70,22 +99,19 @@ const projectInfo = [
     imageAlt: "Screenshot of a wheel of fortune type quiz game",
     info: 'Approached with the mindset of mobile-first, this Javascript project is based upon the classic quiz game "wheel of fortune". It is a two player word guessing game, whereby highest score wins. Incorrect guesses and player turns are handled "automatically" i.e. without user input. Players spin an animated wheel for points and guess letters via the projects purpose built keyboard.',
     imageUrl: quizImg,
-    isReversed: false,
+    isReversed: true,
     techstackIcons: [
       {
-        size: "2xl",
-        icon: "fa-brands fa-html5 fa-inverse",
-        iconColor: "html",
+        viewBox: index("Html5").viewBox,
+        svgInfo: index("Html5").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-js",
-        iconColor: "js",
+        viewBox: index("Javascript").viewBox,
+        svgInfo: index("Javascript").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-sass",
-        iconColor: "sass",
+        viewBox: index("Sass").viewBox,
+        svgInfo: index("Sass").svgInfo
       },
     ],
   },
@@ -96,22 +122,19 @@ const projectInfo = [
     imageAlt: "Screenshot of a Morse code translator application",
     info: 'Tasked with creating a translator app, which adopts an "OOP" approach. Takes in user input for the translation between two languages => English/Morse code and vice-versa.',
     imageUrl: translatorImg,
-    isReversed: true,
+    isReversed: false,
     techstackIcons: [
       {
-        size: "2xl",
-        icon: "fa-brands fa-html5 fa-inverse",
-        iconColor: "html",
+        viewBox: index("Html5").viewBox,
+        svgInfo: index("Html5").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-js",
-        iconColor: "js",
+        viewBox: index("Javascript").viewBox,
+        svgInfo: index("Javascript").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-sass",
-        iconColor: "sass",
+        viewBox: index("Sass").viewBox,
+        svgInfo: index("Sass").svgInfo
       },
     ],
   },
@@ -122,22 +145,19 @@ const projectInfo = [
     imageAlt: "Screenshot of BattleShips OOP Game",
     info: 'A minimalist/retro designed project, written in Vanilla JS, based around the classic battleships game and built according to the principles of the "OOP" paradigm. Created on a grid system, targets hit are visualised by a "flashing animation" and the game is over when the mothership is destroyed.',
     imageUrl: battleshipsImg,
-    isReversed: false,
+    isReversed: true,
     techstackIcons: [
       {
-        size: "2xl",
-        icon: "fa-brands fa-html5 fa-inverse",
-        iconColor: "html",
+        viewBox: index("Html5").viewBox,
+        svgInfo: index("Html5").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-js",
-        iconColor: "js",
+        viewBox: index("Javascript").viewBox,
+        svgInfo: index("Javascript").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-sass",
-        iconColor: "sass",
+        viewBox: index("Sass").viewBox,
+        svgInfo: index("Sass").svgInfo
       },
     ],
   },
@@ -148,22 +168,19 @@ const projectInfo = [
     imageAlt: "Screenshot of a JS Calculator App",
     info: "A working calculator with addition, multiplication, subtraction, division and the added functionality of decimal points and percentages. The calculator has a 'clear' button to reset values and a 'backspace' button to delete the last value entered on screen. Designed in a neomorphic style using Sass, the Bem convention and written in vanilla Javascript.",
     imageUrl: calcImg,
-    isReversed: true,
+    isReversed: false,
     techstackIcons: [
       {
-        size: "2xl",
-        icon: "fa-brands fa-html5 fa-inverse",
-        iconColor: "html",
+        viewBox: index("Html5").viewBox,
+        svgInfo: index("Html5").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-js",
-        iconColor: "js",
+        viewBox: index("Javascript").viewBox,
+        svgInfo: index("Javascript").svgInfo
       },
       {
-        size: "2xl",
-        icon: "fa-brands fa-sass",
-        iconColor: "sass",
+        viewBox: index("Sass").viewBox,
+        svgInfo: index("Sass").svgInfo
       },
     ],
   },
