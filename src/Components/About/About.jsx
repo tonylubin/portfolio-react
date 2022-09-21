4c190370f44a4193b848d3181803d7418f52a96e
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-09-21 12:46:09 +0100

    text update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,10 +8,11 @@ const About = () => {
       <SectionHeader heading="About" />
       <div className={styles.aboutSection__textHolder}>
         <p>
-          Hi there<span className={styles.wave}>&#128075;&#127996;</span>,let me introduce myself briefly, my name is Anthony Lubin and I am currently on a software development bootcamp course with Nology.
+          Hi there<span className={styles.wave}>&#128075;&#127996;</span>, let me introduce myself briefly, my name is Anthony, and with the aim of becoming a developer, I have successfully completed a software development bootcamp course with Nology.
         </p>
         <p>
-          After my university studies, in London and Paris, I went on to establish my own business in the automotive sector. Working in an ever-increasing technology driven environment it became apparent that one had to think along the lines of "why/how the software program is designed/functions?" in order to be effective and efficient (adapt/evolve or fade away).
+          After my university studies, in London and Paris, I went on to establish my own business in the automotive sector. The combination of being "tech-minded" and the soft-skills gained from running a business feels like a skill-set that is well suited to working in the tech across all sectors.<br /> 
+          Working in an ever-increasing technology driven environment it became apparent that I had to think along the lines of "why/how the software program is designed/functions?" in order to be effective and efficient (adapt/evolve or fade away...).
         </p>
         <p>
           This led me to explore the world of coding and, having a passion for tech and languages, with the aspect of building solutions and/or breaking down complex problems down into smaller or simplier processes in that "logic mindest" felt like a great blend of the two in my next career journey.<br />
