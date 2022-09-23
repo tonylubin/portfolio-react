7f69c9e212e467160f92c8b7b25b2ccec992d426
Anthony Lubin <anthonylubin@yahoo.co.uk>
2022-09-23 18:28:42 +0100

    info update

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -46,7 +46,7 @@ const projectInfo = [
     code: "https://github.com/tonylubin/Beer-Finder-Punk-api",
     view: "https://tonylubin.github.io/Beer-Finder-Punk-api",
     title: "BrewDog Beer Finder App",
-    info: 'A project built in React that allows the user to search for Punk beers, by letter/name or filter functionality, via the external Punk beer Api. The results are shown to the user as a flipable "beer card" with extra info about the beer on the back of the card. Project also incorporates, where suitable, unit testing using jest/enzyme.',
+    info: 'A project built in React that allows the user to search for Punk beers, by letter/name or filter functionality, via fetch requests to the Punk Beer API. The results are shown to the user as a flipable "beer card" with extra info about the beer on the back of the card. Project also incorporates, where suitable, unit testing using jest/enzyme.',
     imageUrl: beerImg,
     imageAlt: "Screenshot of a beer finder app using BrewDogs punk beers Api",
     isReversed: true,
@@ -69,7 +69,7 @@ const projectInfo = [
     code: "https://github.com/tonylubin/todos-app",
     view: "https://mytodosapp-4e40f.firebaseapp.com/",
     title: "MyTodos App",
-    info: "An app for a user to keep track of tasks. Built with a connection to Firebase as a BaaS to allow CRUD operations, a user can write, update and delete a todo, as well as being able to show a todo as completed. Furthermore, authentication was added, thus to utilise the app a user must be registered.",
+    info: "An app for a user to keep track of tasks. Built with a connection to Firebase as a BaaS to allow CRUD operations, a user can write, update and delete a todo, as well as being able to show a todo as completed. Furthermore, authentication was added, thus to utilise the app a user must be registered/logged in. This is handled through login/registration form(s) using firebase auth.",
     imageUrl: myTodosImg,
     imageAlt: "Screenshot of a app that deals with todos",
     isReversed: false,
@@ -95,7 +95,7 @@ const projectInfo = [
   {
     code: "https://github.com/tonylubin/JS---Quiz-Game.git",
     view: "https://tonylubin.github.io/JS---Quiz-Game",
-    title: "Spin a Fortune - Javascript Quiz Game ",
+    title: "Spin a Fortune - JS Quiz Game ",
     imageAlt: "Screenshot of a wheel of fortune type quiz game",
     info: 'Approached with the mindset of mobile-first, this Javascript project is based upon the classic quiz game "wheel of fortune". It is a two player word guessing game, whereby highest score wins. Incorrect guesses and player turns are handled "automatically" i.e. without user input. Players spin an animated wheel for points and guess letters via the projects purpose built keyboard.',
     imageUrl: quizImg,
@@ -141,9 +141,9 @@ const projectInfo = [
   {
     code: "https://github.com/tonylubin/JS---Aliens-Game-OOP",
     view: "https://tonylubin.github.io/JS---Aliens-Game-OOP",
-    title: "Alien BattleShips - Javascript OOP Game",
+    title: "Alien BattleShips - JS OOP Game",
     imageAlt: "Screenshot of BattleShips OOP Game",
-    info: 'A minimalist/retro designed project, written in Vanilla JS, based around the classic battleships game and built according to the principles of the "OOP" paradigm. Created on a grid system, targets hit are visualised by a "flashing animation" and the game is over when the mothership is destroyed.',
+    info: 'A minimalist/retro designed project, tasked to be written within 150 lines of code in Vanilla JS. Based around the classic battleships game and built according to the principles of the "OOP" paradigm with added DOM manipulation. Created on a co-ordinated system using CSS Grid, targets hit are visualised by a "flashing animation" and the game is over when the mothership is destroyed.',
     imageUrl: battleshipsImg,
     isReversed: true,
     techstackIcons: [
@@ -164,7 +164,7 @@ const projectInfo = [
   {
     code: "https://github.com/tonylubin/JS---Calculator",
     view: "https://tonylubin.github.io/JS---Calculator",
-    title: "Javascript Calculator App",
+    title: "JS Calculator App",
     imageAlt: "Screenshot of a JS Calculator App",
     info: "A working calculator with addition, multiplication, subtraction, division and the added functionality of decimal points and percentages. The calculator has a 'clear' button to reset values and a 'backspace' button to delete the last value entered on screen. Designed in a neomorphic style using Sass, the Bem convention and written in vanilla Javascript.",
     imageUrl: calcImg,
