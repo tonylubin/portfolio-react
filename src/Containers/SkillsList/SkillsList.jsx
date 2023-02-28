df7a758825e1d1657f66759d8327d053858155c2
Anthony Lubin <anthonylubin@yahoo.co.uk>
2023-02-28 13:03:18 +0000

    added extra skills text

--- src/Containers/SkillsList/SkillsList.jsx
+++ src/Containers/SkillsList/SkillsList.jsx
@@ -18,13 +18,30 @@ const SkillsList = () => {
   ));
 
   return (
-    <section className={styles.skillsList} id="skills">
-      <SectionHeader heading="Skills" />
-      <div className={styles.skillsList__container}>
-        {getSkillsCards}
-      </div>
-    </section>
-  );
+    <>
+      <section className={styles.skillsList} id="skills">
+        <SectionHeader heading="Skills" />
+        <div className={styles.skillsList__container}>
+          {getSkillsCards}
+        </div>
+      </section>
+      <section className={styles.skillsExtra}>
+        <h3>FRONTEND</h3>
+        <p><span className={styles.skillsExtra__info}>HTML5:</span> including semantic html and accessibility best practices</p>
+        <p><span className={styles.skillsExtra__info}>CSS3:</span> responsive design, Flexbox, CSS Grid, SASS, CSS Modules & Frameworks</p>
+        <p><span className={styles.skillsExtra__info}>JAVASCRIPT:</span> ES6+, Asynchronous & Functional programming, OOP(Classes), RESTful API's, DOM manipulation, JSON</p>
+        <p><span className={styles.skillsExtra__info}>REACT:</span> hooks, context, functional components</p>
+        <h3>BACKEND</h3>
+        <p><span className={styles.skillsExtra__info}>NODE.JS:</span> vanilla, Express, EJS</p>
+        <p><span className={styles.skillsExtra__info}>DATABASES:</span> NoSQL - MongoDB & Mongoose, Cloud Firestore</p>
+        <p><span className={styles.skillsExtra__info}>FIREBASE:</span> hosting & authentication</p>
+        <h3>FUNDAMENTALS</h3>
+        <p><span className={styles.skillsExtra__info}>VERSION CONTROL:</span> Git & Github</p>
+        <p><span className={styles.skillsExtra__info}>UNIT TESTING:</span> Jest & Enzyme</p>
+        <p><span className={styles.skillsExtra__info}>TOOLS:</span> Figma, Postman, Trello, CLI, VS code</p>
+      </section>
+    </>
+  );    
 };
 
 export default SkillsList;
