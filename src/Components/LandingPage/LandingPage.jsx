156c5ee2c417a0e4382188bc5dc9170887e87b0f
Anthony Lubin <anthonylubin@yahoo.co.uk>
2024-07-22 16:32:40 +0100

    text correction

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
           onInit={(typewriter) => {
             typewriter.pauseFor(50).changeDelay(85)
             .typeString("<span style='font-weight: 700; font-size: clamp(1.9rem, 5vw, 2.5rem);'>Hello</span>, (tech world),</br>")
-            .typeString("<span style='font-weight: 700';'>I'm Anthony Lubin, </span>a junior software developer based in London. </br>")
+            .typeString("<span style='font-weight: 700';'>I'm Anthony Lubin, </span>a software developer based in London. </br>")
             .typeString("</br><span style='font-weight: 700;'>Please </span>scroll down to</br>")
             .typeString("<span style='font-weight: 700;'>Enter</span>.....")
             .start();
